deaf949ee59084389aaafd5af7b9f533223c23bc
chandhu <chandhu.mpc19@gmail.com>
2022-07-28 05:11:44 +0530

    connecting db to mongodb atlas

--- databaseConnect.js
+++ databaseConnect.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 exports.connect=async ()=>{
 try{
-    await mongoose.connect(process.env.DB_URL,{
+    await mongoose.connect(process.env.DB_URL1,{
         useNewUrlParser:true,
         useUnifiedTopology: true,
     });
