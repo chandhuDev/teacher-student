a616ce8870cf8fb8377783b15e93177125240328
chandhu <chandhu.mpc19@gmail.com>
2022-08-03 00:58:45 +0530

    first commit

--- passport/passport.js
+++ passport/passport.js
@@ -19,7 +19,7 @@ const PORT=process.env.PORT || 5000
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://student-teachers-app.herokuapp.com:PORT/register"
+    callbackURL: "https://student-teachers-app.herokuapp.com/register"
   },
   function(accessToken, refreshToken, profile, next) {
     
